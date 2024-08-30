5b0b243a37d4752231bb0d7a1012ee842585ea9a
chrod <chrod.dev@gmail.com>
2024-08-30 12:29:00 -0500

    Add usePlazasContext hook

---
+++ frontend/src/hooks/usePlazasContext.jsx
@@ -0,0 +1,6 @@
+import { useContext } from 'react';
+import PlazasContext from '../contexts/plazas/PlazasContext';
+
+const usePlazasContext = () => useContext(PlazasContext);
+
+export default usePlazasContext;
