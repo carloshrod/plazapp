da7b6349c46891ae5c3ca9317bfe35d3cdc5a511
chrod <chrod.dev@gmail.com>
2024-09-23 18:43:01 -0500

    Add backend production endpoints

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,6 +1,11 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
+const isDev = import.meta.env.NODE_ENV === 'development';
 
 export const env = {
-	REGISTER_USER_ENDPOINT: `${BASE_URL}/registerUser`,
-	UPDATE_USER_ENDPOINT: `${BASE_URL}/updateUser`,
+	REGISTER_USER_ENDPOINT: isDev
+		? `${BASE_URL}/registerUser`
+		: import.meta.env.VITE_API_REGISTER_USER_ENDPOINT,
+	UPDATE_USER_ENDPOINT: isDev
+		? `${BASE_URL}/updateUser`
+		: import.meta.env.VITE_API_UPDATE_USER_ENDPOINT,
 };
